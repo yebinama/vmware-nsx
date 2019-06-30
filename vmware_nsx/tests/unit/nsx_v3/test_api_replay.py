0891e9ab6c6eb9600ed3f8d20302895eca8ebc14
Adit Sarfaty <asarfaty@vmware.com>
2019-06-30 13:50:06 +0300

    NSX|V3 migration: Fix _fixup_res_dict for api_replay
    The path of _fixup_res_dict changed to neutron_lib
    In additon, router gateway should not be skipped.
    
    Change-Id: I0317da8689d7c798f1cf0da70e8f6ed3e0567e2d

--- vmware_nsx/tests/unit/nsx_v3/test_api_replay.py
+++ vmware_nsx/tests/unit/nsx_v3/test_api_replay.py
@@ -21,7 +21,7 @@ from neutron_lib.plugins import directory
 from oslo_config import cfg
 
 
-class TestApiReplay(test_plugin.NsxV3PluginTestCaseMixin):
+class TestApiReplay(test_plugin.L3NatTest):
 
     def setUp(self, plugin=None, ext_mgr=None, service_plugins=None):
         # enables api_replay_mode for these tests
@@ -61,3 +61,34 @@ class TestApiReplay(test_plugin.NsxV3PluginTestCaseMixin):
                                      id=specified_port_id)
         port = self.deserialize(self.fmt, port_res)
         self.assertEqual(specified_port_id, port['port']['id'])
+
+    def _create_router(self, fmt, tenant_id, name=None,
+                       admin_state_up=None,
+                       arg_list=None, **kwargs):
+        data = {'router': {'tenant_id': tenant_id}}
+        if name:
+            data['router']['name'] = name
+        if admin_state_up:
+            data['router']['admin_state_up'] = admin_state_up
+        for arg in (('admin_state_up', 'tenant_id') + (arg_list or ())):
+            # Arg must be present and not empty
+            if kwargs.get(arg):
+                data['router'][arg] = kwargs[arg]
+        router_req = self.new_create_request('routers', data, fmt)
+        return router_req.get_response(self.ext_api)
+
+    def test_create_update_router(self):
+        specified_router_id = '555e762b-d7a1-4b44-b09b-2a34ada56c9f'
+        router_res = self._create_router(self.fmt,
+                                         'test-tenant',
+                                         'test-rtr',
+                                         arg_list=('id',),
+                                         id=specified_router_id)
+        router = self.deserialize(self.fmt, router_res)
+        self.assertEqual(specified_router_id, router['router']['id'])
+
+        # This part tests _fixup_res_dict as well
+        body = self._update('routers', specified_router_id,
+                            {'router': {'name': 'new_name'}})
+        body = self._show('routers', specified_router_id)
+        self.assertEqual(body['router']['name'], 'new_name')
